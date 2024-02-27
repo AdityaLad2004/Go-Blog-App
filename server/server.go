975f0466efb0d523de1ce9242954754a2e82694f
adityalad2004 <aditya.lad.2004@gmail.com>
2024-02-27 23:07:33 +0530

    added mysql connection and done with blog model

--- server/server.go
+++ server/server.go
@@ -3,10 +3,22 @@ package main
 import (
 	"log"
 
+	"github.com/AdityaLad2004/blog/database"
 	"github.com/gofiber/fiber/v2"
 )
 
+func init() {
+	database.ConnectDB()
+}
 func main() {
+
+	sqlDB, err := database.DBConn.DB()
+
+	if err != nil {
+		panic("Eror in sql conn")
+	}
+
+	defer sqlDB.Close()
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
